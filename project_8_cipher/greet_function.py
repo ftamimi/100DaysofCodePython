aed678aee4a89562c9931f5867646b9146308662
farrukhtamimi <farrukh.tamimi@gmail.com>
2026-02-21 14:52:39 +0500

    first commit of cipher

---
+++ project_8_cipher/greet_function.py
@@ -0,0 +1,18 @@
+# def greet():
+#     print('Hello')
+#     print('Wishing you a great day')
+#     print('Take care')
+
+# greet()
+
+# def greet_with_name(name):
+#     print(f'Hi {name}')
+#     print(f'How do you do {name}?')
+
+# greet_with_name('Farrukh')
+
+def greet_with_name_and_loc(name, location):
+    print(f'Hello {name}')
+    print(f'What is it like in {location}?')
+
+greet_with_name_and_loc(location='London', name='Farrukh')
